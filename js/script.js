1e0809acddc890c4ef1897606a852258e1e44162
iNata <natandreychenko@gmail.com>
2016-04-25 19:36:11 +0300

    ADD student to team function added

--- js/script.js
+++ js/script.js
@@ -92,11 +92,24 @@ $(document).ready(function () {
     var el = $(this);
     var removedValue = el.closest('.student__item').attr('id');       
     $('#'+removedValue).remove();
+    var removedId = removedValue.split('-').pop();
     
+    // remove also from team lists
     $('.student__team-list li').each(function(){
-      if($(this).text(removedValue)){
+      var teamStudentIdAttr = $(this).attr('id');
+      var teamStudentId = teamStudentIdAttr.split('-').pop();
+      if(teamStudentId == removedId){
         $(this).remove();
       }
+    });
+    
+    // remove also from student team table
+    $('.student__team-table tbody tr').each(function (){        
+        var teamRowIdAttr = $(this).attr('id');        
+        var teamRowId = teamRowIdAttr.split('-').pop();
+        if(teamRowId == removedId){
+          $(this).remove();
+        }           
     });
     
     $('.student__team-select option[value="'+removedValue+'"]').remove();
@@ -111,34 +124,36 @@ $(document).ready(function () {
   function addToTeam(){
     
     var el = $(this).closest('.student__team');
-    
-    var selectedStudent = el.find('.student__team-select :selected').text();
+    var selectedOption = el.find('.student__team-select :selected');    
+    var selectedStudent = selectedOption.text();    
     var selectedValue = el.find('.student__team-select').val();
+    var selectedId = selectedValue.split('-').pop();
 
-    el.find('.student__team-select :selected').remove();
+    selectedOption.remove();
     
-    var studentTeam = el.find('.student__team-list');
-    
-    var studentTeamChild = '<li>'+ selectedStudent +'</li>';
+    var studentTeam = el.find('.student__team-list'); 
+    var studentTeamId = studentTeam.attr('id');
+    var studentTeamChild = '<li id="' + studentTeamId + '-' + selectedId + '">' + selectedStudent + '</li>';
     studentTeam.append(studentTeamChild);
+
+    // find all team lists and remove selected value    
+    $('.student__team-select option').each(function(){
+      var eachEl = $(this);
+      var optionValue = eachEl.val();
+      var optionId = optionValue.split('-').pop();
+      if(optionId == selectedId){
+        eachEl.remove();
+      }
+    });
     
     // insert table row
     var table = el.find('.student__team-table tbody');
-    var tr = table.append($('<tr>'));
+    var tr = table.append($('<tr id="'+studentTeamId+'Row-'+selectedId+'">'));
     var j;
     for (j = 0; j < 5; j++){
       table.find('tr:last-child').append($('<td></td>'))
-    }
-    
-    table.find('tr:last-child td:first-child').text(selectedStudent);
-    
-    // find all team lists and remove selected value
-    
-    $('.student__team-select option').each(function(){
-       if($(this).val() == selectedValue){
-         $(this).remove();
-       } 
-    })
+    }    
+    table.find('tr:last-child td:first-child').text(selectedStudent); 
     
   }
   
